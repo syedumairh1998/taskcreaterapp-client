cb67cb0fdca055575a5ed81f5352496bf16e87f7
syedumairhussain1998 <syedumairh1998@gmail.com>
2024-01-30 02:24:28 +0500

    created login signup and resetpassword screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import SignUp from "./screens/Signup";
+import ForgetPassword from "./screens/ResetPassword";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Login} />
+        <Route path="/signup" Component={SignUp} />
+        <Route path="/reset-password" Component={ForgetPassword} />
+      </Routes>
+    </Router>
   );
 }
 
